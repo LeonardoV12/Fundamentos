8cb8900dfbe71c7736450cee1165c6cdf59df2d8
LeonardoV12 <110415095+LeonardoV12@users.noreply.github.com>
2022-08-15 21:32:41 -0400

    Grupo 5

---
+++ grupo 5.cpp
@@ -0,0 +1,27 @@
+// Grupo 6.cpp : Defines the entry point for the console application.
+//
+
+include "stdafx.h"
+#include <iostream>
+using namespace std;
+void main ()
+
+float x1, x2, y1, y2, h, k;
+cout<< "Ingresar el valor x1"<<endl;
+cin>> x1>> endl;
+cout<< "ingresar valor de x2<< endl;\
+cin>> x2>> endl;
+cout<< "Ingresar el valor y1"<<endl;
+cin>> y1 >>ndl;
+cout<< "Ingresar el valor y2<<endl;
+cin>> y2 >>ndl;
+cout<< "Ingresar el valor h<<endl;
+cin>> h >>ndl;
+cout<< "Ingresar el valor k<<endl;
+cin>> k  >>endl;
+r= (x-h)*(x-h)+(y-k)*(y-k);
+cout<< "El radio es'<<endl;
+m= (x1+x2)/2 + (y1+y2)/2
+cout<< "El punto medio es:"<<endl;
+return 0;
+}
