50b6dd627c848fec78917d260c6b7709d1307ad1
LeonardoV12 <110415095+LeonardoV12@users.noreply.github.com>
2022-08-15 20:59:43 -0400

    Add files via upload

---
+++ grupo 6.cpp
@@ -0,0 +1,18 @@
+// Grupo 6.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+ {
+int A, B, X;
+cout<<"Hola usuario" <<endl;
+cout<<"Ingrese valor de A:"<<endl;
+cin    
+cou<<"Ingrese valor de B:
+cin>>B;
+X=A, A=B, B=x;
+cout<‹"El valor de A es:
+cout<<"El valor de B es:
+return 0;
+}
